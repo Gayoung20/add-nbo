b8e2ba98d3e80eb5bd421875535e67fbad06eaff
Gayoung20 <ga2894@korea.ac.kr>
2021-09-09 08:12:24 -0400

    Add add modules

---
+++ add.cpp
@@ -0,0 +1,9 @@
+// add.cpp
+#include "add.h"
+#include <stdint.h>
+
+uint32_t add(uint32_t a, uint32_t b)
+{
+	uint32_t c = a + b;
+	return c;
+}
---
+++ add.h
@@ -0,0 +1,5 @@
+// add.h
+#pragma once
+#include <stdint.h>
+
+uint32_t add(uint32_t a, uint32_t b);
